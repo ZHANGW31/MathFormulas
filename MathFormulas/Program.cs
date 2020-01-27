12363fee4ce6459681d70ceca9afbea9140fbd0d
Wei Zhang <wei.zhang4@outlook.com>
2020-01-27 14:34:53 -0800

    Updated all of the methods and added exception handling

--- MathFormulas/Program.cs
+++ MathFormulas/Program.cs
@@ -10,82 +10,171 @@ namespace MathFormulas
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Wei's math formula calculator! ");
+            Console.WriteLine("\nPlease select from the following.");
+            Console.WriteLine("\nI want to...");
+            Console.WriteLine("\n(1)Calculate the circumference and Area of the circle given the radius");
+            Console.WriteLine("(2)Calculate the volume of the hemisphere given the radius");
+            Console.WriteLine("(3)Calculate the Area of the triangle given the length of the sides");
+            Console.WriteLine("(4)Solve a quadratic equation");
+            Console.WriteLine("\nPlease press type the corresponding letter and press enter!");
 
+            try
+            {
+                var userSelection = InitialInputValue();
 
+                if (userSelection == 1)
+                {
+                    Console.WriteLine("You chose to calculate the circumference and area of the circle.");
+                    Console.WriteLine("We now need a number from you for the radius");
+                    int circleInput = ValidateUserInput();
+                    Console.WriteLine($"The Area of the circle is: {AreaOfCircle(circleInput)}");
+                    Console.WriteLine($"The circumference of the circle is {CircleCircumference(circleInput)}");
+                }
+                if (userSelection == 2)
+                {
+                    int hemisphereInput = ValidateUserInput();
+                    Console.WriteLine($"The volume of the hemisphere is: {Hemisphere(hemisphereInput)}");
+                }
+                if (userSelection == 3)
+                {
+                    Console.WriteLine("\nPlease enter the first side!");
+                    int triangleInputOne = ValidateUserInput();
+                    Console.WriteLine("Please enter the second side!");
+                    int triangleInputTwo = ValidateUserInput();
+                    Console.WriteLine("Please enter the third side!");
+                    int triangleInputThree = ValidateUserInput();
+
+                    Console.WriteLine($"The area of the triangle is {AreaOfTriangle(triangleInputOne, triangleInputTwo, triangleInputThree)}");
+                }
+                if (userSelection == 4)
+                {
+                    Console.WriteLine("Please enter a value for a");
+                    int valueA = ValidateUserInput();
+                    Console.WriteLine("Please enter a value for b");
+                    int valueB = ValidateUserInput();
+                    Console.WriteLine("Please enter a value for c");
+                    int valueC = ValidateUserInput();
+
+                    QuadraticFormula(valueA, valueB, valueC);
+                }
+
+            } catch (Exception)
+            {
+                Console.WriteLine("An unexpected error has occured!");
+            }
+            finally
+            {
+                Console.WriteLine("\nExiting Application");
+            }
         }
 
-        static double CircleCircumference(double radius)
+        static int InitialInputValue()
         {
-            Console.WriteLine("\nPart 1, circumference and area of a circle.");
-            Console.Write("Enter an integer for the radius to find the circumference of the circle: ");
-            //This line allows for user input in the console.
-            string strradius = Console.ReadLine();
-            //int.Parse takes the user input and converts it into a integar which then allows for the code to assign it as an int
-            int intradius = int.Parse(strradius);
-            double circumference = 2 * Math.PI * intradius;
-            Console.WriteLine($"The circumference is {circumference}");
+            Console.Write("\nPlease enter your choice: ");
+            bool isValid = false;
+            string fromUser = Console.ReadLine();
+            int output = 0;
+            while (isValid == false)
+            {
+                if (!int.TryParse(fromUser, out output))
+                {
+                    Console.WriteLine("{0} is not an input", fromUser);
+                    Console.WriteLine("Please enter a valid value");
+                    fromUser = Console.ReadLine();
+                    continue;
+                }
 
+                if(output > 4)
+                {
+                    Console.WriteLine("{0} is not a right choice", fromUser);
+                    Console.WriteLine("Please enter a valid value");
+                    fromUser = Console.ReadLine();
+                    continue;
+                }
+                if (output == 0)
+                {
+                    Console.WriteLine("{0} is not a right choice", fromUser);
+                    Console.WriteLine("Please enter a valid value");
+                    fromUser = Console.ReadLine();
+                    continue;
+                }
+                else
+                {
+
+                    isValid = true;
+                }
+            }
+
+            return output;
+        }
+
+        //This method tests to user input and validates that it is an integer
+        static int ValidateUserInput()
+        {
+            Console.Write("\nYour Value: ");
+            bool isValid = false;
+            string fromUser = Console.ReadLine();
+            int output = 0;
+            while (isValid == false)
+            {
+                if (!int.TryParse(fromUser, out output))
+                {                   
+                    Console.WriteLine("{0} is not a valid integer", fromUser);
+                    Console.WriteLine("Please enter a valid value!");
+                    fromUser = Console.ReadLine();
+                    continue;
+                }
+
+                else
+                {
+                    isValid = true;
+                }
+            }
+
+
+            return output;
+        }
+
+        static double CircleCircumference(int radius)
+        {
+            double circumference = 2 * Math.PI * radius;
             return circumference;
         }
 
         //Part 1 Area of Circle implementation here.
-        static double AreaOfCircle(double radius)
+        static double AreaOfCircle(int radius)
         {
-            Console.Write("Enter an integer for the radius to find the area of the circle: ");
-            var strradius = Console.ReadLine();
-            int intradius = int.Parse(strradius);
-            double area = Math.PI * (intradius * intradius);
-            Console.WriteLine($"The area is {area}");
-
+            double area = Math.PI * (Math.Pow(radius, 2));
             return area;
         }
 
         // Part 2 Volume of Hemisphere implementation here.
-        static double Hemisphere(double radius)
+        static double Hemisphere(int radius)
         {
-            Console.WriteLine("\nPart 2, volume of a hemisphere.");
-            Console.Write("Enter an integer for the radius to find the volume of the hemisphere: ");
-            var strradius = Console.ReadLine();
-            int intradius = int.Parse(strradius);
+            int intradius = radius;
 
             // Formulas for r cubed, and the volume.
             double rCubed = (intradius * intradius * intradius);
             double volume = (4.0 / 3.0) * Math.PI * rCubed / 2;
-            Console.WriteLine($"The volume is {volume}");
 
             return volume;
         }
 
         // Part 3 Area of Triangle implementation here.
-        static double AreaOfTriangle(double sides)
+        static double AreaOfTriangle(int sideOne, int sideTwo, int sideThree)
         {
 
+            var a = sideOne;
+            var b = sideTwo;
+            var c = sideThree;
 
-            Console.WriteLine("\nPart 3, Area of a triangle given the length of the sides");
-            Console.Write("Enter an integer for the lenth of the first side:");
-            string strtriangleSideOne = Console.ReadLine();
-            Console.Write("Enter an integer for the length of the second side:");
-            string strtriangleSideTwo = Console.ReadLine();
-            Console.Write("Enter an integer for the length of the third side:");
-            string strtriangleSideThree = Console.ReadLine();
-
-            // Takes user input for each side of the triangle and parses the string and converts it into an integer.
-
-            double inttriangleSideOne = int.Parse(strtriangleSideOne);
-            double inttriangleSideTwo = int.Parse(strtriangleSideTwo);
-            double inttriangleSideThree = int.Parse(strtriangleSideThree);
-
-            double a = inttriangleSideOne;
-            double b = inttriangleSideTwo;
-            double c = inttriangleSideThree;
-
-            // Formulas to identify the half of the circumference and the area of the triangle.
 
             double p = (a + b + c) / 2;
 
             double triangleArea = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
 
-            Console.WriteLine($"The area of the triangle is {triangleArea}");
+            
 
             return triangleArea;
 
@@ -95,27 +184,16 @@ namespace MathFormulas
 
         // Implementation here
 
-        static double QuadraticFormula(double values)
+        static double QuadraticFormula(int a, int b, int c)
         {
-            Console.WriteLine("\nPart 4, solving a quadratic equation.");
-            Console.WriteLine("Please enter an integer for a:");
-            string astr = Console.ReadLine();
-            Console.WriteLine("Please enter the integer for b:");
-            string bstr = Console.ReadLine();
-            Console.WriteLine("Please enter the integer for c:");
-            string cstr = Console.ReadLine();
 
-            double anum = int.Parse(astr);
-            double bnum = int.Parse(bstr);
-            double cnum = int.Parse(cstr);
-
-            double positive_num = (bnum * -1.0) + Math.Sqrt(Math.Pow(bnum, 2) - 4.0 * anum * cnum);
-            double negative_num = (bnum * -1.0) - Math.Sqrt(Math.Pow(bnum, 2) - 4.0 * anum * cnum);
-            double denominator = 2.0 * anum;
+            double positive_num = ((double)b * -1.0) + Math.Sqrt(Math.Pow((double)b, 2) - 4.0 * (double)a * (double)c);
+            double negative_num = ((double)b * -1.0) - Math.Sqrt(Math.Pow((double)b, 2) - 4.0 * (double)a * (double)c);
+            double denominator = 2.0 * (double)a;
 
 
-            Console.WriteLine($"The positive solution is {positive_num / denominator}");
-            Console.WriteLine($"The negative solution is {negative_num / denominator}");
+            Console.WriteLine($"\nThe positive solution is {positive_num / denominator}");
+            Console.WriteLine($"\nThe negative solution is {negative_num / denominator}");
 
             return positive_num;
         }
